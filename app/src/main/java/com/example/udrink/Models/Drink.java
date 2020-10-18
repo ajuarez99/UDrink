a04efc2e202146df46bbdab369f44dbb45513e96
Allan Juarez <allanjuarez86@gmail.com>
2020-10-18 17:29:14 -0500

    add drink model

---
+++ app/src/main/java/com/example/udrink/Models/Drink.java
@@ -0,0 +1,11 @@
+package com.example.udrink.Models;
+
+import java.util.Date;
+
+public class Drink {
+    private String DrinkName;
+    public int uid;
+    public Date DrankAt;
+    public int BAC;
+    public int Ounces;
+}
