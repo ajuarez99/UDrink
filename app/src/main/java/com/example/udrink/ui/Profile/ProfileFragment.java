0d07ef9714ae5779aaa81f2de5449e1c564d1f70
Joseph Strobel <j.strobelight@yahoo.com>
2020-10-22 16:15:12 -0500

    started on profile page

--- app/src/main/java/com/example/udrink/ui/Profile/ProfileFragment.java
+++ app/src/main/java/com/example/udrink/ui/Profile/ProfileFragment.java
@@ -1,15 +1,17 @@
 package com.example.udrink.ui.Profile;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
-import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.core.graphics.drawable.IconCompatParcelizer;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.udrink.R;
@@ -18,18 +20,19 @@ public class ProfileFragment extends Fragment {
 
     private ProfileViewModel dashboardViewModel;
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         dashboardViewModel =
                 ViewModelProviders.of(this).get(ProfileViewModel.class);
         View root = inflater.inflate(R.layout.fragment_profile, container, false);
-        final TextView textView = root.findViewById(R.id.profile);
-        dashboardViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+
+        final TextView nameView = root.findViewById(R.id.nameTextView);
+        final ImageView profileImage = root.findViewById(R.id.profileImage);
+
+        nameView.setText("Joe Strobel");
+        profileImage.setImageResource(R.drawable.circle);
+
         return root;
     }
 }
