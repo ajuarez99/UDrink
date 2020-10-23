bf07a3d0d69423a5d46bd82198f8c1b7db09c362
Allan Juarez <allanjuarez86@gmail.com>
2020-10-23 10:29:21 -0500

    added more user fields

---
+++ app/src/main/java/com/example/udrink/Firebase/FirebaseUtil.java
@@ -0,0 +1,18 @@
+package com.example.udrink.Firebase;
+
+import com.example.udrink.Models.User;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class FirebaseUtil {
+    private DatabaseReference mDatabase;
+    public FirebaseUtil() {
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+    }
+
+    private void writeNewUser(String userId, String name, String email) {
+        User user = new User(name, email);
+
+        mDatabase.child("users").child(userId).setValue(user);
+    }
+}
--- app/src/main/java/com/example/udrink/Models/User.java
+++ app/src/main/java/com/example/udrink/Models/User.java
@@ -1,14 +1,55 @@
 package com.example.udrink.Models;
 
 public class User {
-    private int uid;
+    private String uid;
     private String name;
+    private int weight;
+    private int feet;
+    private int inches;
 
-    public int getUid() {
+    public User(String uid, String name, int weight, int feet, int inches) {
+        this.uid = uid;
+        this.name = name;
+        this.weight = weight;
+        this.feet = feet;
+        this.inches = inches;
+    }
+
+    public User(String uid, String name, int weight) {
+        this.uid = uid;
+        this.name = name;
+        this.weight = weight;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public int getFeet() {
+        return feet;
+    }
+
+    public void setFeet(int feet) {
+        this.feet = feet;
+    }
+
+    public int getInches() {
+        return inches;
+    }
+
+    public void setInches(int inches) {
+        this.inches = inches;
+    }
+
+    public String getUid() {
         return uid;
     }
 
-    public void setUid(int uid) {
+    public void setUid(String uid) {
         this.uid = uid;
     }
 
